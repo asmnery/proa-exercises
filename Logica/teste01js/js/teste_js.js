b3988caa4772fa512385c068502df6e6dd4ebedc
Alberth Nery <107318708+asmnery@users.noreply.github.com>
2025-09-03 10:19:47 -0300

    adicionado exercicios de logica

---
+++ Logica/teste01js/js/teste_js.js
@@ -0,0 +1,23 @@
+function exibe_rx7() {
+    document.getElementById("rx7").style.visibility="visible";
+    document.getElementById("supra").style.visibility="hidden";
+    document.getElementById("evo").style.visibility="hidden";
+}
+
+function exibe_supra() {
+    document.getElementById("rx7").style.visibility="hidden";
+    document.getElementById("supra").style.visibility="visible";
+    document.getElementById("evo").style.visibility="hidden";
+}
+
+function exibe_evo() {
+    document.getElementById("rx7").style.visibility="hidden";
+    document.getElementById("supra").style.visibility="hidden";
+    document.getElementById("evo").style.visibility="visible";
+}
+
+function some_tudo() {
+    document.getElementById("rx7").style.visibility="hidden";
+    document.getElementById("supra").style.visibility="hidden";
+    document.getElementById("evo").style.visibility="hidden";
+}
